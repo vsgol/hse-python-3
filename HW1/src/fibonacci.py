b7cde876abd8eabd9ea042beb788f41d9df36888
Valery Golovin <galloDest@gmail.com>
2022-02-03 00:11:50 +0300

    HW 1

--- HW1/src/fibonacci.py
+++ HW1/src/fibonacci.py
@@ -1,5 +1,7 @@
 def fibonacci(n):
-    res = [0, 1]
-    for _ in range(n-1):
-        res.append(res[-1] + res[-2])
-    return res[1:n+1]
+    fib_seq = [0, 1]
+    for _ in range(n - 1):
+        prev = fib_seq[-1]
+        prev2 = fib_seq[-2]
+        fib_seq.append(prev + prev2)
+    return fib_seq[1:n + 1]
