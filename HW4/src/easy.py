6567df0d8bb83a18b06248d2fe57cd3020612396
Valery Golovin <galloDest@gmail.com>
2022-03-11 21:25:20 +0100

    HW 4 easy artefacts

--- HW4/src/easy.py
+++ HW4/src/easy.py
@@ -8,9 +8,9 @@ def timing(f):
     @wraps(f)
     def wrap(*args, **kw):
         ts = time()
-        f(*args, **kw)
+        res = f(*args, **kw)
         te = time()
-        return te - ts
+        return res, te - ts
 
     return wrap
 
@@ -43,11 +43,11 @@ def main():
     workers_num = 10
     n = 10 ** 5
 
-    sync_time = run_sync(fibonacci, n, workers_num)
-    threads_time = run_workers(fibonacci, n, workers_num, Thread)
-    process_time = run_workers(fibonacci, n, workers_num, Process)
-    with open('artifacts/easy_statistics.txt', 'w') as f:
-        f.write(f'Test get the first {n} Fibonacci numbers {workers_num} times')
+    sync_time = run_sync(fibonacci, n, workers_num)[1]
+    threads_time = run_workers(fibonacci, n, workers_num, Thread)[1]
+    process_time = run_workers(fibonacci, n, workers_num, Process)[1]
+    with open('../artifacts/easy_statistics.txt', 'w') as f:
+        f.write(f'Test get the first {n} Fibonacci numbers {workers_num} times\n')
         f.write(f'Sync time: {sync_time} sec\n')
         f.write(f'Threads time: {threads_time} sec\n')
         f.write(f'Process time: {process_time} sec\n')
